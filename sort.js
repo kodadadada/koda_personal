824f515080ebb4873e62050b43cf6797abc448e6
kodadadada <y.hayakawa1231@gmail.com>
2023-12-15 19:00:04 +0900

    ソートを追加

--- sort.js
+++ sort.js
@@ -1,13 +1,9 @@
 var sortNumber = function (number) {
     number.sort(function (a, b) {
- <<<<<<< HEAD
-       if (a === b) {
- =======
-       if (a == b) {
- >>>>>>> 839396c5383737ec06b9c2a842bfccc28f3996ef
-          return 0;
-       }
-       return a < b ? -1 : 1;
+        if (a == b) {
+            return 0;
+        }
+        return a < b ? -1 : 1;
     });
  };
  
