4dbff4d8fd53fd0827da7907a5d86d1a0071a31a
kodadadada <y.hayakawa1231@gmail.com>
2023-12-15 18:53:11 +0900

    競合を解決

--- sort.js
+++ sort.js
@@ -1,9 +1,13 @@
 var sortNumber = function (number) {
     number.sort(function (a, b) {
-        if (a == b) {
-            return 0;
-        }
-        return a < b ? -1 : 1;
+ <<<<<<< HEAD
+       if (a === b) {
+ =======
+       if (a == b) {
+ >>>>>>> 839396c5383737ec06b9c2a842bfccc28f3996ef
+          return 0;
+       }
+       return a < b ? -1 : 1;
     });
  };
  
